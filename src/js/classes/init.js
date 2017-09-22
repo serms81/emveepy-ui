cace031427a24d46dbd1a03c77fe4854a5442cce
Sergio Micó <smico@rankia.com>
2017-09-22 16:10:12 +0200

    Img Zoom

--- src/js/classes/init.js
+++ src/js/classes/init.js
@@ -12,27 +12,41 @@ class Init {
     alert(txt)
   }
 
-  onload () {
-    document.addEventListener(
-      'DOMContentLoaded',
-      function () {
-        let body = document.querySelector('body')
-        body.querySelectorAll('img[zoomable]').forEach((img) => { Ooh.zoomImg(img) })
-        body.querySelector('#theme-toggler').addEventListener('click', () => { body.classList.toggle('theme-dark') })
 
-        let list = document.getElementById('main-nav-bar').querySelector('ul')
-        let sections = document.querySelectorAll('section[id]')
-
-        sections.forEach((item) => {
-          let anchor = document.createElement('a')
-          let li = document.createElement('li')
-          li.textContent = item.title
-          anchor.href = `#${item.id}`
-          anchor.appendChild(li)
-          list.appendChild(anchor)
-        })
+  zoomImg (target) {
+    target.parentElement.style.cursor = 'zoom-in'
+    target.style.cursor = 'inherit'
+    target.addEventListener(
+      'click',
+      function (ev) {
+        var target = ev.target
+        var wrapper = document.createElement('div')
+        var clone = document.createElement('img')
+        clone.src = target.src
+        clone.classList.add('img-zoomed');
+        wrapper.classList.add('img-zoomed-wrapper');
+        wrapper.appendChild(clone)
+        document.body.appendChild(wrapper)
+        wrapper.addEventListener('click', function () { this.parentElement.removeChild(this) })
       }
     )
+  }
+
+  onload () {
+    window.zoomImg = this.zoomImg
+    document.body.querySelector('#theme-toggler').addEventListener('click', () => { document.body.classList.toggle('theme-dark') })
+
+    let list = document.getElementById('main-nav-bar').querySelector('ul')
+    let sections = document.querySelectorAll('section[id]')
+
+    sections.forEach((item) => {
+      let anchor = document.createElement('a')
+      let li = document.createElement('li')
+      li.textContent = item.title
+      anchor.href = `#${item.id}`
+      anchor.appendChild(li)
+      list.appendChild(anchor)
+    })
   }
 }
 
