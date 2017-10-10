2618b9374c6adb2eafbc65f07b52a872b024ace3
serms81 <serms81@gmail.com>
2017-10-10 06:28:44 +0200

    WIP

--- src/js/classes/init.js
+++ src/js/classes/init.js
@@ -1,4 +1,5 @@
 import Ooh from './ooh.js'
+var MenuSpy = require('menuspy')
 
 class Init {
 
@@ -6,12 +7,6 @@ class Init {
     this.title1 = 'ES6 + webpack'
     this.title2 = 'is working!'
   }
-
-  displayMessage () {
-    const txt = `${this.title1}, ${this.title2}`
-    alert(txt)
-  }
-
 
   zoomImg (target) {
     target.parentElement.style.cursor = 'zoom-in'
@@ -23,8 +18,8 @@ class Init {
         var wrapper = document.createElement('div')
         var clone = document.createElement('img')
         clone.src = target.src
-        clone.classList.add('img-zoomed');
-        wrapper.classList.add('img-zoomed-wrapper');
+        clone.classList.add('img-zoomed')
+        wrapper.classList.add('img-zoomed-wrapper')
         wrapper.appendChild(clone)
         document.body.appendChild(wrapper)
         wrapper.addEventListener('click', function () { this.parentElement.removeChild(this) })
@@ -36,17 +31,29 @@ class Init {
     window.zoomImg = this.zoomImg
     document.body.querySelector('#theme-toggler').addEventListener('click', () => { document.body.classList.toggle('theme-dark') })
 
-    let list = document.getElementById('main-nav-bar').querySelector('ul')
+    let list = document.body.querySelector('#section-links')
     let sections = document.querySelectorAll('section[id]')
 
     sections.forEach((item) => {
       let anchor = document.createElement('a')
       let li = document.createElement('li')
       li.textContent = item.title
+      li.classList.add('nav-bar-item')
+      anchor.classList.add('nav-bar-link')
       anchor.href = `#${item.id}`
       anchor.appendChild(li)
       list.appendChild(anchor)
     })
+    var ms = new MenuSpy(
+      list,
+      {
+        menuItemSelector: '.nav-bar-link',
+        activeClass: 'nav-bar-link--active',
+        threshold: 15,
+        hashTimeout: 600,
+        callback: () => { console.log(ms) }
+      }
+    )
   }
 }
 
